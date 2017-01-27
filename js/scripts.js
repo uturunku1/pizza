c02173af0bd0b3a8d9f45cfaba952ec41040da39
stella huayhuaca <luces.huayhuaca@gmail.com>
2017-01-27 13:34:42 -0800

    this commits the object constructor/prototype for pizza object.

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,13 @@
-function returncostpizza(toppings,size) {
+function pizza(toppings,size) {
+  this.toppings=toppings;
+  this.size= size;
+  if (toppings.length===0) {
+    return "It is required to select at least one topping."
+  }
+
+}
+
+function returnCost(toppings,size) {
   if (toppings.length===4) {
     var costtoppings= 8;
   }else if (toppings.length===3) {
@@ -7,27 +16,20 @@ function returncostpizza(toppings,size) {
     costtoppings= 4;
   }else if (toppings.length===1){
     costtoppings = 2;
-  }else {
-    return costpizza="Is required to select at least one topping."
   }
   if (size==="small") {
-    return costpizza = costtoppings + 10;
+    return costtoppings + 10;
   }else if (size==="medium") {
-    return costpizza = costtoppings + 15;
+    return costtoppings + 15;
   } else {
-    return costpizza =  costtoppings + 20;
+    return costtoppings + 20;
   }
+
 }
 
-
-// function pizza(toppings,size) {
-//   this.toppings=toppings;
-//   this.size= size;
-// }
-//
-// pizza.prototype.cost(function() {
-//   return "The cost of your pizza(s) is " + costpizza;
-// })
+pizza.prototype.reviewcost = function() {
+  return "The pizza you want to purchase is " + this.size + " size, with the following toppings: "+ this.toppings+". The total cost is " + this.totalcost +" dollars." ;
+}
 
 
 
@@ -43,9 +45,12 @@ $(document).ready(function() {
       //$("#outputCost").append(toppings+"<br>");
     })
     var size = $("input:radio[name=size]:checked").val();
-//    var newpizza= new pizza(inputtedToppings,inputtedsize)
-    var costpizza = returncostpizza(toppings,size);
-    $("#outputCost").text(costpizza);
+    var newpizza= new pizza(toppings,size);
+    var cost = returnCost(toppings,size);
+    newpizza.totalcost = cost;
+
+
+    $("#outputCost").text(newpizza.reviewcost());
 
 
   })
